7da1c617d7ed3c5e5161bb22cc7aa26dec2e9f9e
Gayashan <gayashan@cse.mrt.ac.lk>
2015-11-16 17:07:52 +0530

    Added log4j logging support.

--- src/main/java/lk/ac/mrt/cse/dbs/samples/JDBCSample.java
+++ src/main/java/lk/ac/mrt/cse/dbs/samples/JDBCSample.java
@@ -15,16 +15,18 @@
 
 package lk.ac.mrt.cse.dbs.samples;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
  * This class demonstrates the use of mysql-jdbc-connector driver to access a MySQL database.
  */
 public class JDBCSample {
+    private static final Logger log = LogManager.getLogger(JDBCSample.class);
     private static final String MYSQL_JDBC_DRIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
     private static final String MYSQL_DATABASE_URL = "jdbc:mysql://localhost:3306/dbs_tryit";
     private static final String USERNAME = "root";
@@ -37,15 +39,15 @@ public class JDBCSample {
             connection = DriverManager.getConnection(MYSQL_DATABASE_URL, USERNAME, PASSWORD);
 
         } catch (ClassNotFoundException e) {
-            System.err.println("Unable to find the driver classes: " + e.getLocalizedMessage());
+            log.error("Unable to find the driver classes: ", e);
         } catch (SQLException e) {
-            System.err.println("Unable to access the database server: " + e.getLocalizedMessage());
+            log.error("Unable to access the database server: ", e);
         } finally {
             if (connection != null) {
                 try {
                     connection.close();
                 } catch (SQLException e) {
-                    System.err.println("Unable to close the database connection: " + e.getLocalizedMessage());
+                    log.error("Unable to close the database connection: ", e);
                 }
             }
         }
