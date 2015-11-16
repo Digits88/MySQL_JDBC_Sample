acd20978b2cb386d4432e0bedde82c8af1f7cd78
Gayashan <gayashan@cse.mrt.ac.lk>
2015-11-16 16:48:39 +0530

    Refactored some variables.

--- src/main/java/lk/ac/mrt/cse/dbs/samples/JDBCSample.java
+++ src/main/java/lk/ac/mrt/cse/dbs/samples/JDBCSample.java
@@ -1,17 +1,26 @@
 package lk.ac.mrt.cse.dbs.samples;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * This class demonstrates the use of mysql-jdbc-connector driver to access a MySQL database.
  */
 public class JDBCSample {
+    private static final String MYSQL_JDBC_DRIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
+    private static final String MYSQL_DATABASE_URL = "jdbc:mysql://localhost:3306/dbs_tryit";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
 
     public static void main(String[] args) {
         Connection connection = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbs_tryit", "root", "");
+            Class.forName(MYSQL_JDBC_DRIVER_CLASS_NAME);
+            connection = DriverManager.getConnection(MYSQL_DATABASE_URL, USERNAME, PASSWORD);
+
         } catch (ClassNotFoundException e) {
             System.err.println("Unable to find the driver classes: " + e.getLocalizedMessage());
         } catch (SQLException e) {
